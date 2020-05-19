fb3da1b49f740f83347197f6b413c81da0bef855
Ainthient <50053516+Ainthient@users.noreply.github.com>
2020-05-19 19:44:57 +0300

    Add files via upload

---
+++ MoscowTransport.WebService/DomainObjects/Ports/IBIKESobjRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace BIKESobj.DomainObjects.Ports
+{
+    public interface IReadOnlyBIKESobjRepository
+    {
+        Task<BIKESobj> GetBIKESobj(long id);
+
+        Task<IEnumerable<BIKESobj>> GetAllBIKESobjs();
+
+        Task<IEnumerable<BIKESobj>> QueryBIKESobj(ICriteria<BIKESobj> criteria);
+
+    }
+
+    public interface IBIKESobjRepository
+    {
+        Task AddBIKESobj(BIKESobj bikesObj);
+
+        Task RemoveBIKESobj(BIKESobj bikesObj);
+
+        Task UpdateBIKESobj(BIKESobj bikesObj);
+    }
+}
